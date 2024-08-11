de2b3d428eda37b96cf8f2e07032b2eea9319ac7
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-08-11 14:28:55 +0530

    Add ip function

--- app/Services/Global/Functions.php
+++ app/Services/Global/Functions.php
@@ -32,3 +32,18 @@ function dd(...$args) {
     }
     die;
 }
+
+function userIP() {
+  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    // Check if from shared internet
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+  } elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    // Check if from a proxy
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  } else {
+    // Default, REMOTE_ADDR
+    $ip = $_SERVER['REMOTE_ADDR'];
+  }
+  
+  return $ip;
+}
