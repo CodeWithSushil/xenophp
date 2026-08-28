6d8bfeea3f2d697e46be3408c83484f71bf3f04b
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-08-28 15:46:33 +0530

    add php.ini values

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,10 +2,10 @@
 
 namespace Tests;
 
+use App;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
-use App;
 
 #[CoversClass(App::class)]
 class BaseTest extends TestCase
